dd9aea281c72b82ba989f71d06ed239ae9e2b04d
Mads Johansen <mads@madsfoto.dk>
2017-06-27 11:17:37 +0200

    Comments + a little error handling

--- ParallelTest1-ForEach/Program.cs
+++ ParallelTest1-ForEach/Program.cs
@@ -13,7 +13,18 @@ namespace ParallelTest1_ForEach
 
             // Set the maximum parallel executions via an argument. The other option would be to hardcode it, which I am not a fan of. 
             // The third option is to do (1) "Convert.ToInt32(Math.Ceiling((Environment.ProcessorCount * 0.75) * 1.0))", making use of 75% of the total processer usage. 
-            string str = args[0];
+            // Currently this does not work. 
+            string str = null;
+            if (args[0] == null)
+            {
+                args[0] = "3";
+                str = args[0];
+            }
+            else
+            {
+                str = args[0];
+            }
+            
             int maxParallelExecutions = Convert.ToInt32(str); // I am currently unable to take an int directly from the argument list, hence the conversion here
             // Crashes if no argument is given
 
@@ -39,10 +50,30 @@ namespace ParallelTest1_ForEach
 
                 proc.StartInfo.FileName = currentFile; // Set the currentfile as the one being executed. Incrementing automatically.
 
-                proc.Start();
-                proc.WaitForExit();  // Another key point: As we are executing a external batch file, this program thinks that it can start as many bat files as it wants,
-                // leading to excessive cpu and ram usage. 
+                
 
+                proc.Start();
+                Console.WriteLine("started " + currentFile);
+
+                // if (proc.waitforexit throws exception, delete file)
+
+                try
+                {
+                    proc.WaitForExit();  // Another key point: As we are executing a external batch file, this program thinks that it can start as many bat files as it wants,
+                                         // leading to excessive cpu and ram usage. 
+                }
+                catch (DivideByZeroException e) // An exception is thrown when the bat file gives an error.
+                // The underlaying problem is that I generate the bat files by going from a 'start number' to 'start number + n' without checking if 'start number + n' is a valid filename
+                // I let imagemagick handle that error (by giving a console file not found error), and just move on.
+                
+                {
+                    File.Delete(filename); // This is the "The only tool I have is a hammer" approach. I am sure there are smarter and better ways to catch the exceptions
+                    // but for the purposes of this program, this is fine. 
+                    // Since the bat files are single use and can be remade, it does not matter that they are deleted. The files in here are not worth spending time on anyway. 
+                }
+                
+
+                File.Delete(filename);
             });
 
 
