4e817421c3ab79546d533dd062b10b77e4e8dd1e
Mads Johansen <mads@madsfoto.dk>
2017-07-02 18:18:36 +0200

    More try/catch testing (didni't work)

--- ParallelTest1-ForEach/Program.cs
+++ ParallelTest1-ForEach/Program.cs
@@ -50,19 +50,20 @@ namespace ParallelTest1_ForEach
 
                 proc.StartInfo.FileName = currentFile; // Set the currentfile as the one being executed. Incrementing automatically.
 
-                
-
-                proc.Start();
-                Console.WriteLine("started " + currentFile);
-
-                // if (proc.waitforexit throws exception, delete file)
 
                 try
                 {
-                    proc.WaitForExit();  // Another key point: As we are executing a external batch file, this program thinks that it can start as many bat files as it wants,
+                    proc.Start();
+                Console.WriteLine("started " + currentFile);
+
+                // if (proc.waitforexit throws exception, ignore and delete file)
+
+                
+                    proc.WaitForExit();
+                    //proc.WaitForExit(2147483647);  // Another key point: As we are executing a external batch file, this program thinks that it can start as many bat files as it wants,
                                          // leading to excessive cpu and ram usage. 
                 }
-                catch (DivideByZeroException e) // An exception is thrown when the bat file gives an error.
+                catch (AggregateException) // An exception is thrown when the bat file gives an error.
                 // The underlaying problem is that I generate the bat files by going from a 'start number' to 'start number + n' without checking if 'start number + n' is a valid filename
                 // I let imagemagick handle that error (by giving a console file not found error), and just move on.
                 
@@ -71,8 +72,69 @@ namespace ParallelTest1_ForEach
                     // but for the purposes of this program, this is fine. 
                     // Since the bat files are single use and can be remade, it does not matter that they are deleted. The files in here are not worth spending time on anyway. 
                 }
-                
+                catch (UnauthorizedAccessException)
+                {
+                    File.Delete(filename);
+                    // test
+                }
+                catch (InvalidOperationException)
+                {
+                    File.Delete(filename);
+                }
+                // First level error given is :
+                /*
+   Unhandled Exception: System.AggregateException: One or more errors occurred. ---> System.InvalidOperationException: No process is associated with this object.
+   at System.Diagnostics.Process.EnsureState(State state)
+   at System.Diagnostics.Process.EnsureState(State state)
+   at System.Diagnostics.Process.GetProcessHandle(Int32 access, Boolean throwIfExited)
+   at System.Diagnostics.Process.WaitForExit(Int32 milliseconds)
+   at System.Diagnostics.Process.WaitForExit()
+   at ParallelTest1_ForEach.Program.<>c__DisplayClass0_0.<Main>b__0(String currentFile)
+   at System.Threading.Tasks.Parallel.<>c__DisplayClass30_0`2.<ForEachWorker>b__0(Int32 i)
+   at System.Threading.Tasks.Parallel.<>c__DisplayClass17_0`1.<ForWorker>b__1()
+   at System.Threading.Tasks.Task.InnerInvoke()
+   at System.Threading.Tasks.Task.InnerInvokeWithArg(Task childTask)
+   at System.Threading.Tasks.Task.<>c__DisplayClass176_0.<ExecuteSelfReplicating>b__0(Object )
+   
+   --- End of inner exception stack trace ---
+   at System.Threading.Tasks.Task.ThrowIfExceptional(Boolean includeTaskCanceledExceptions)
+   at System.Threading.Tasks.Task.Wait(Int32 millisecondsTimeout, CancellationToken cancellationToken)
+   at System.Threading.Tasks.Task.Wait()
+   at System.Threading.Tasks.Parallel.ForWorker[TLocal](Int32 fromInclusive, Int32 toExclusive, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, 
+   Func`4 bodyWithLocal, Func`1 localInit, Action`1 localFinally)
+   at System.Threading.Tasks.Parallel.ForEachWorker[TSource,TLocal](TSource[] array, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, 
+   Action`3 bodyWithStateAndIndex, Func`4 bodyWithStateAndLocal, Func`5 bodyWithEverything, Func`1 localInit, Action`1 localFinally)
+   at System.Threading.Tasks.Parallel.ForEachWorker[TSource,TLocal](IEnumerable`1 source, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, 
+   Action`3 bodyWithStateAndIndex, Func`4 bodyWithStateAndLocal, Func`5 bodyWithEverything, Func`1 localInit, Action`1 localFinally)
+   at System.Threading.Tasks.Parallel.ForEach[TSource](IEnumerable`1 source, ParallelOptions parallelOptions, Action`1 body)
+   at ParallelTest1_ForEach.Program.Main(String[] args)
 
+                Second level error: 
+                Unhandled Exception: System.AggregateException: One or more errors occurred. ---> System.UnauthorizedAccessException: Access to the path 'E:\Pharrell Williams - Happy (1AM)\52945.b
+at' is denied.
+   at System.IO.__Error.WinIOError(Int32 errorCode, String maybeFullPath)
+   at System.IO.File.InternalDelete(String path, Boolean checkHost)
+   at System.IO.File.Delete(String path)
+   at ParallelTest1_ForEach.Program.<>c__DisplayClass0_0.<Main>b__0(String currentFile)
+   at System.Threading.Tasks.Parallel.<>c__DisplayClass30_0`2.<ForEachWorker>b__0(Int32 i)
+   at System.Threading.Tasks.Parallel.<>c__DisplayClass17_0`1.<ForWorker>b__1()
+   at System.Threading.Tasks.Task.InnerInvoke()
+   at System.Threading.Tasks.Task.InnerInvokeWithArg(Task childTask)
+   at System.Threading.Tasks.Task.<>c__DisplayClass176_0.<ExecuteSelfReplicating>b__0(Object )
+   --- End of inner exception stack trace ---
+   at System.Threading.Tasks.Task.ThrowIfExceptional(Boolean includeTaskCanceledExceptions)
+   at System.Threading.Tasks.Task.Wait(Int32 millisecondsTimeout, CancellationToken cancellationToken)
+   at System.Threading.Tasks.Task.Wait()
+   at System.Threading.Tasks.Parallel.ForWorker[TLocal](Int32 fromInclusive, Int32 toExclusive, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, Func`4 bodyW
+ithLocal, Func`1 localInit, Action`1 localFinally)
+   at System.Threading.Tasks.Parallel.ForEachWorker[TSource,TLocal](TSource[] array, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, Action`3 bodyWithStateA
+ndIndex, Func`4 bodyWithStateAndLocal, Func`5 bodyWithEverything, Func`1 localInit, Action`1 localFinally)
+   at System.Threading.Tasks.Parallel.ForEachWorker[TSource,TLocal](IEnumerable`1 source, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, Action`3 bodyWithS
+tateAndIndex, Func`4 bodyWithStateAndLocal, Func`5 bodyWithEverything, Func`1 localInit, Action`1 localFinally)
+   at System.Threading.Tasks.Parallel.ForEach[TSource](IEnumerable`1 source, ParallelOptions parallelOptions, Action`1 body)
+   at ParallelTest1_ForEach.Program.Main(String[] args)
+
+                */
                 File.Delete(filename);
             });
 
