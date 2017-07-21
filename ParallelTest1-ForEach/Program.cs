a9b10af644c0a3d6f2f6f5446b237ca9dbdb108d
Mads Johansen <mads@madsfoto.dk>
2017-07-21 19:24:27 +0200

    Prefer x64 && have a lower default amount of files executed at any given time with argument overwrite

--- ParallelTest1-ForEach/Program.cs
+++ ParallelTest1-ForEach/Program.cs
@@ -7,20 +7,31 @@ namespace ParallelTest1_ForEach
 {
     class Program
     {
-        public int currentCount = 1;
-        public int maxCount = 1;
+        public int currentCount = 0;
+        public int maxCount = 0;
 
         public void setMaxCount(int count)
         {
             maxCount = count;
         }
 
-        public float percentDone()
+        public string percentDone()
         {
             float currentCountFloat = currentCount;
-            float percentdone = ((currentCountFloat / maxCount)*100);
+            float percentdone = (currentCountFloat / maxCount);
+            string percStr = percentdone.ToString("P");
+            return percStr;
+        }
 
-            return percentdone;
+        public int imagesLeft()
+        {
+            int imgsLeft = maxCount - currentCount;
+            return imgsLeft;
+        }
+
+        public int imgsDone()
+        {
+            return currentCount;
         }
 
         public void execbat(string fileName)
@@ -47,41 +58,64 @@ namespace ParallelTest1_ForEach
             int count = paths.Length;
 
             p.setMaxCount(count);
+            
+            int numberOfMaxDegreeOfParallelismInt = 1;
 
+            // Taken from https://stackoverflow.com/a/11791998/7963551
+
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("The default is to have 3 parallel executions at once, overwrite with a commandline argument" );
+                numberOfMaxDegreeOfParallelismInt = 3; // Set to 3, which is 75% of a quadcore machine which I expect is the minimum amount of cores,
+                // that people who are interested in this program will have. I give the posibility to overwrite the default by giving an argument, 
+                // so people with more cores can use them all at once.
+            }
+            else
+            {
+                
+                string strMaxDegrees = args[0];
+                numberOfMaxDegreeOfParallelismInt = Convert.ToInt32(strMaxDegrees);
+                Console.WriteLine("Executing " + numberOfMaxDegreeOfParallelismInt + " files at once");
+            }
 
             // Set the maximum parallel executions via an argument. The other option would be to hardcode it, which I am not a fan of. 
             // The third option is to do (1) "Convert.ToInt32(Math.Ceiling((Environment.ProcessorCount * 0.75) * 1.0))", making use of 75% of the total processer usage. 
-            // Currently this does not work. 
-            // string str = args[0];
-            
-            // int maxParallelExecutions = Convert.ToInt32(str); // I am currently unable to take an int directly from the argument list, hence the conversion here
-            // Crashes if no argument is given
 
-
+           
             // The magic is in the foreach statement: 
             // For each of the files in the list, execute the 'current file' in effect the next in the list.
             // While no more than maxParallelExecutions is running at the same time
 
-            Parallel.ForEach(paths, new ParallelOptions { MaxDegreeOfParallelism = 6 }, // use the line from (1) .
+            //Parallel.ForEach(paths, new ParallelOptions { MaxDegreeOfParallelism = 6 }, // use the line from (1) . // orginal line
+
+            Parallel.ForEach(paths, new ParallelOptions { MaxDegreeOfParallelism = numberOfMaxDegreeOfParallelismInt }, 
                 (currentFile) =>
             {
                 String fileName = Path.GetFileName(currentFile); // Test if filename is required, can currentFile be used?
 
-                Console.WriteLine("started " + currentFile);
+                // Console.WriteLine("started " + currentFile);  // Disabled because it does not matter to the viewer which file is being executed,
+                // The progress indicator is the important thing.
                 p.execbat(currentFile); // moved the executing logic to a function, so it's self contained and thus will not generate the exceptions seen before. 
 
                 Interlocked.Increment(ref p.currentCount);
 
-                // Before I've had some "how far are we" logic, but it didn't work. 
+                Console.WriteLine("percent done = " + p.percentDone() + " | Images to go = " + p.imagesLeft() + " | Images done this session = " + p.imgsDone());
+                // TODO: Time remaning. 
+                //
+                /*
+                Stopwwatch start at execbat(), stop after execution => How long does one file tale
+                Time for 1 file * leftovers (imagesLeft() ) == Time left. 
+                However the time is not always uniform, so we need a average time.
+                UNTESTED: 
+                Put stopwatch elapsedtime into variable, restart timer, execute, stop timer, put elapsed time into variable. 
+                Then after incremented, do time taken / imgsDone() == average time taken * imgsLeft() == Time remaining.
+                This way the counter is more accurate as time goes on. 
 
 
-                // if (proc.waitforexit throws exception, ignore and delete file)
 
 
 
-                //proc.WaitForExit(2147483647);  // Another key point: As we are executing a external batch file, this program thinks that it can start as many bat files as it wants,
-                // leading to excessive cpu and ram usage. 
-                Console.WriteLine("percent done = " + p.percentDone());
+                */
                 File.Delete(currentFile);
             });
 
